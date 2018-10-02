e65f6ab8c7f06a43cb7635e14ee54860103bc074
Kenji Fukuda <fkenji@fukuta-no-macbook-pro.local>
2018-10-02 15:49:45 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Kenji Fukuda on 2018/10/02.
+//  Copyright © 2018年 Kenji Fukuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
